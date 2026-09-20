c656ea1b7c1ecedcfc0cc680fa4c2f38f841643a
SHAURYA JAIN <supershaurya23@gmail.com>
2026-09-20 14:55:08 +0530

    day27

---
+++ day27/ques2.c
@@ -0,0 +1,55 @@
+// Q54: Write a program to print the following pattern:
+
+   /*
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+   
+    for (i = 1; i <= 7; i += 2) {
+
+        for (j = 1; j <= (7 - i) / 2; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+   
+    for (i = 5; i >= 1; i -= 2) {
+
+        for (j = 1; j <= (7 - i) / 2; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
